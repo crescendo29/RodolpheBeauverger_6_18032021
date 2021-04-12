3c4f9aaec278482ef3cfc7286e0a120dd350f8c6
beauverger <beauverger.rodolphe@orange.fr>
2021-04-12 09:46:00 +0200

    sécurité

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
+const mongoSanitize = require("express-mongo-sanitize"); //nettoie les data utilisateur pour prévenir l'injection d'opérateurs mongoDB
+const helmet = require("helmet"); //Helmet aide à protéger l'application de certaines des vulnérabilités bien connues du Web en configurant de manière appropriée des en-têtes HTTP.
+const hpp = require("hpp"); // middleware qui protège contre la pollution des paramètres HTTP
+
 const userRoutes = require("./routes/user");
 const sauceRoutes = require("./routes/sauce");
 
@@ -16,6 +20,8 @@ mongoose
 
 const app = express();
 
+app.use(helmet());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
@@ -23,7 +29,12 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(mongoSanitize());
+
+app.use(hpp());
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/auth/", userRoutes);
